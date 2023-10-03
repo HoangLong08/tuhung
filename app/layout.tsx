5a11965ef3d296a5eadc332cbf73d939f748828a
HoangLong08 <longnguyen.080400@gmail.com>
2023-10-03 20:08:21 +0700

    add vercel"

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Sarabun } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 import MainLayout from '@/layouts';
 import './suneditor.css';
+import Head from 'next/head';
 
 const sarabun = Sarabun({ subsets: ['latin'], weight: '500' });
 
@@ -15,18 +16,30 @@ export const metadata: Metadata = {
   },
   description:
     'Chúng tôi là nhà phân phối thép hàng đầu Việt Nam, đáp ứng dịch vụ Phong Phú - Chất Lượng - Tiến Độ - Rộng Khắp.',
-  keywords: 'thép công trình, thép chất lượng, thép Tứ Hưng',
+  keywords: 'thép công trình, thép chất lượng, thép tứ hưng',
   authors: [
     {
       url: '',
       name: 'Thép Tứ Hưng',
     },
   ],
+  openGraph: {},
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='vi'>
+      <Head>
+        <meta
+          property='og:image'
+          content='https://theptuhung.com.vn/favicon.ico'
+        />
+        <meta property='og:title' content='Your Title' />
+        <meta
+          property='og:description'
+          content='Chúng tôi là nhà phân phối thép hàng đầu Việt Nam, đáp ứng dịch vụ Phong Phú - Chất Lượng - Tiến Độ - Rộng Khắp..'
+        />
+      </Head>
       <body className={sarabun.className}>
         <StyledComponentsRegistry>
           <MainLayout>{children}</MainLayout>
